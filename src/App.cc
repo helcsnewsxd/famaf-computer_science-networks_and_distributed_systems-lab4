435d35143f5cba53ebeb7b3df820899d52b34289
helcsnewsxd <emanuel.nicolas.herrador@unc.edu.ar>
2023-06-02 12:47:11 -0300

    Estructura base del proyecto. Se considera el formato optimo para hacer varias simulaciones y el considerado por Omnet++

--- src/App.cc
+++ src/App.cc
@@ -1,29 +1,21 @@
-#ifndef APP
-#define APP
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
 
-#include <string.h>
-#include <omnetpp.h>
-#include <packet_m.h>
-
-using namespace omnetpp;
-
-class App: public cSimpleModule {
-private:
-    cMessage *sendMsgEvent;
-    cStdDev delayStats;
-    cOutVector delayVector;
-public:
-    App();
-    virtual ~App();
-protected:
-    virtual void initialize();
-    virtual void finish();
-    virtual void handleMessage(cMessage *msg);
-};
+#include "App.h"
 
 Define_Module(App);
-
-#endif /* APP */
 
 App::App() {
 }
---
+++ src/Net.cc
@@ -0,0 +1,48 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "Net.h"
+
+Define_Module(Net);
+
+Net::Net() {
+}
+
+Net::~Net() {
+}
+
+void Net::initialize() {
+}
+
+void Net::finish() {
+}
+
+void Net::handleMessage(cMessage *msg) {
+
+    // All msg (events) on net are packets
+    Packet *pkt = (Packet *) msg;
+
+    // If this node is the final destination, send to App
+    if (pkt->getDestination() == this->getParentModule()->getIndex()) {
+        send(msg, "toApp$o");
+    }
+    // If not, forward the packet to some else... to who?
+    else {
+        // We send to link interface #0, which is the
+        // one connected to the clockwise side of the ring
+        // Is this the best choice? are there others?
+        send(msg, "toLnk$o", 0);
+    }
+}
